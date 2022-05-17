2837143438511f97210c0e980659d974e09ab7f0
Yanis Andry <yanis.andry@evaneos.com>
2022-05-17 18:53:15 +0200

    add constraints on form data

--- src/Form/Data/FeedbackData.php
+++ src/Form/Data/FeedbackData.php
@@ -1,7 +1,21 @@
 <?php
 
+use Symfony\Component\Validator\Constraints;
+
 class FeedbackData
 {
+    /**
+     * @Constraints\Length(
+     * min = 2,
+     * max = 50
+     * )
+     */
     public string $name;
+    /**
+     * @Constraints\Length(
+     * min = 1,
+     * max = 4000
+     * )
+     */
     public string $message;
 }
