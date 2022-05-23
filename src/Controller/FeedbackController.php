74308993cf39b596e7bede8048386f11df1cbe40
Yanis Andry <yanis.andry@evaneos.com>
2022-05-23 11:04:15 +0200

    added a note for the poor souls who'll try to run this code

--- src/Controller/FeedbackController.php
+++ src/Controller/FeedbackController.php
@@ -1,5 +1,18 @@
 <?php
 
+//Dear programmer:
+//When I wrote this code, only god and
+//I knew how it worked.
+//Now only god knows it!
+//
+//Therefore, if you are trying to optimize
+//this routine and it fails (most surely),
+//please increase this counter as a
+//warning for the next person:
+//
+//total_hours_wasted_here = 254
+//
+
 namespace App\Controller;
 
 use App\Form\Type\FeedbackType;
